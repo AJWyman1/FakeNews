8ae50027ee375209f4eb5945d3cfd0be162ca76a
Alex Wyman <awyman93@gmail.com>
2020-08-27 12:45:55 -0400

    added models and imgs

--- src/model_builder.py
+++ src/model_builder.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction import text
 from sklearn.metrics import plot_confusion_matrix
+from sklearn.svm import LinearSVC, SVC
 
 np.random.seed(8675309)
 
@@ -19,7 +20,7 @@ np.random.seed(8675309)
 
 if __name__ == "__main__":
 
-    my_stop_words = text.ENGLISH_STOP_WORDS.union(['reuters'])
+    my_stop_words = text.ENGLISH_STOP_WORDS.union(['reuters', 'com', 'pic'])
     
     naive = MultinomialNB()
 
@@ -27,9 +28,9 @@ if __name__ == "__main__":
     count_vect = CountVectorizer(lowercase=True, stop_words=my_stop_words)
 
 
-    fake_df = pd.read_csv('data/Fake.csv')
+    fake_df = pd.read_csv('data/Fake_sentiment.csv')
     fake_df['y'] = np.ones(fake_df.shape[0], dtype='int')
-    true_df = pd.read_csv('data/True.csv')
+    true_df = pd.read_csv('data/True_sentiment.csv')
     true_df['y'] = np.zeros(true_df.shape[0], dtype='int')
 
     df = pd.concat([fake_df, true_df])
@@ -37,11 +38,10 @@ if __name__ == "__main__":
     corpus = df[['text', 'y']]
     X = corpus.text
     y = corpus.y
-    # holdout = ['WATCH: BLM and Militia Come Together, Discuss How Police May Have Provoked Kenosha Violence']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.5)
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
 
     count_vect.fit(X_train)
-
     X_train_counts = count_vect.transform(X_train)
     X_test_counts = count_vect.transform(X_test)
 
@@ -49,11 +49,21 @@ if __name__ == "__main__":
     X_train_tfidf = vectorizer.transform(X_train_counts)
     X_test_tfidf = vectorizer.transform(X_test_counts)
 
+
+    print("Start of model training")
+
+
     nb_model = MultinomialNB(alpha=1.0, fit_prior=True, class_prior=None)
     nb_model.fit(X_train_tfidf, y_train)
     print(nb_model.score(X_test_tfidf, y_test))
 
-    n = 17
+
+    lin_clf = LinearSVC()
+    lin_clf.fit(X_train_tfidf, y_train)
+    print(lin_clf.score(X_test_tfidf, y_test))
+    
+
+    n = 25
 
     feature_words = count_vect.get_feature_names()
     log_prob_fake = nb_model.feature_log_prob_[1]
@@ -81,7 +91,7 @@ if __name__ == "__main__":
 #         print(title)
 #         print(disp.confusion_matrix)
 
-# plt.show()
+#   plt.show()
 
     importances = rando_forest.feature_importances_
     std = np.std([rando_forest.feature_importances_ for tree in rando_forest.estimators_],
@@ -94,13 +104,25 @@ if __name__ == "__main__":
     for f in range(n):
         print(f"{f + 1}. feature {feature_words[indices[f]]} ({importances[indices[f]]})")
 
+
     # Plot the impurity-based feature importances of the forest
-    plt.figure()
-    plt.title("Feature importances")
-    plt.bar(range(n), importances[indices],
-            color="r", yerr=std[indices], align="center")
-    plt.xticks(range(n), indices)
-    plt.xlim([-1, n])
-    plt.show()
+    # plt.figure()
+    # plt.title("Feature importances")
+    # plt.bar(range(n), importances[indices],
+    #         color="r", yerr=std[indices], align="center")
+    # plt.xticks(range(n), indices)
+    # plt.xlim([-1, n])
+    # plt.show()
 
     # print(feature_words)
+
+    sentiment_corpus = df[['com', 'neu', 'pos', 'neg', 'y']]
+
+    X_sent = sentiment_corpus[['com', 'neu', 'pos', 'neg']]
+    y_sent = sentiment_corpus.y
+    X_train_sent, X_test_sent, y_train_sent, y_test_sent = train_test_split(X_sent, y_sent)
+
+    rf_class = RandomForestClassifier()
+    rf_class.fit(X_train_sent, y_train_sent)
+    print(rf_class.score(X_test_sent, y_test_sent))
+
--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -4,14 +4,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
+import sys
 
 plt.style.use('ggplot')
 
 class sentiment_analysis(object):
 
-    def __init__(self, fake_df, true_df):
+    def __init__(self, fake_df, true_df, is_vader=True):
         self.fake_df = fake_df
         self.true_df = true_df
+        self.is_vader = is_vader
 
     def get_sentiment(self, title, analyzer):
         vs = analyzer.polarity_scores(title)
@@ -23,7 +25,7 @@ class sentiment_analysis(object):
         
         return com, neu, pos, neg
 
-    def append_four(self, lst1, lst2, lst3, lst4, app1, app2, app3, app4, remove_defaults=True):
+    def append_four(self, lst1, lst2, lst3, lst4, app1, app2, app3, app4, remove_defaults=False):
         if remove_defaults:
             if app1:
                 lst1.append(app1)
@@ -38,45 +40,75 @@ class sentiment_analysis(object):
             lst3.append(app3)
             lst4.append(app4)
 
+    def append_and_save_df(self, df, path, row, analyzer, verbose=False):
+        com, neu, pos, neg = self.analyze_df(df, row, analyzer, verbose=verbose)
+        # breakpoint()
+        df['com'] = com
+        df['neu'] = neu
+        df['pos'] = pos
+        df['neg'] = neg
+
+        df.to_csv(path)
+
+    def analyze_df(self, df, row, analyzer, verbose=False):
+        com = []
+        neu = []
+        pos = []
+        neg = []
+
+        for text in range(0, df.shape[0]):
+                if verbose:
+                    print(f'\rANALYZING TEXT NUMBER {text}')
+
+                title = df[row][text]
+                
+                _com, _neu, _pos, _neg = self.get_sentiment(title, analyzer)
+                # breakpoint()
+                self.append_four(com, neu, pos, neg, _com, _neu, _pos, _neg)
+
+        return com, neu, pos, neg
+
     def analyze_and_plot(self, row, analyzer, verbose=False):
-        fake_com = []
-        fake_neu = []
-        fake_pos = []
-        fake_neg = []
+        if self.is_vader:
+            fake_com = []
+            fake_neu = []
+            fake_pos = []
+            fake_neg = []
 
-        true_com = []
-        true_neu = []
-        true_pos = []
-        true_neg = []
+            true_com = []
+            true_neu = []
+            true_pos = []
+            true_neg = []
 
 
-        for text in range(0, fake_df.shape[0], 1):
-            if verbose:
-                print(f'ANALYZING TEXT NUMBER {text}')
+            for text in range(0, 20000, 1):
+                if verbose:
+                    print(f'\rANALYZING TEXT NUMBER {text}')
 
-            fake_title = self.fake_df[row][text]
-            #true_title = self.true_df[row][text]
+                fake_title = self.fake_df[row][text]
+                true_title = self.true_df[row][text]
 
-            f_com, f_neu, f_pos, f_neg = self.get_sentiment(fake_title, analyzer)
-            #t_com, t_neu, t_pos, t_neg = self.get_sentiment(true_title, analyzer)
+                f_com, f_neu, f_pos, f_neg = self.get_sentiment(fake_title, analyzer)
+                t_com, t_neu, t_pos, t_neg = self.get_sentiment(true_title, analyzer)
+                
+
+                self.append_four(fake_com, fake_neu, fake_pos, fake_neg, f_com, f_neu, f_pos, f_neg)
+                self.append_four(true_com, true_neu, true_pos, true_neg, t_com, t_neu, t_pos, t_neg)
+                sys.stdout.flush()
+
             
+            fake_attributes = [fake_com, fake_neu, fake_pos, fake_neg]
+            true_attributes = [true_com, true_neu, true_pos, true_neg]
+            col_names = ['com', 'neu', 'pos', 'neg']
 
-            self.append_four(fake_com, fake_neu, fake_pos, fake_neg, f_com, f_neu, f_pos, f_neg)
-            #self.append_four(true_com, true_neu, true_pos, true_neg, t_com, t_neu, t_pos, t_neg)
+            #self.fake_df = self.add_columns(self.fake_df, fake_attributes, col_names)
+            print(self.fake_df.head())
 
-        
-        fake_attributes = [fake_com, fake_neu, fake_pos, fake_neg]
-        #true_attributes = [true_com, true_neu, true_pos, true_neg]
-        col_names = ['com', 'neu', 'pos', 'neg']
+            fig, axs = plt.subplots(4,2, sharey=True, sharex='row')
 
-        self.fake_df = self.add_columns(self.fake_df, fake_attributes, col_names)
-        print(self.fake_df.head())
+            num_bins = 30
 
-        fig, axs = plt.subplots(4,2, sharey='row', sharex='row')
-
-        num_bins = 30
-
-        #self.plotter(fake_attributes, true_attributes, axs, num_bins)
+            self.plotter(fake_attributes, true_attributes, axs, num_bins)
 
     def add_columns(self, df, attributes, cols):
         for col, rows in zip(cols, attributes):
@@ -85,7 +117,8 @@ class sentiment_analysis(object):
             df[col] = rows
         return df
 
-
+    def boot_strap(self, df):        
+        return np.random.choice(df.shape[0])
 
     def plotter(self, fake_attributes, true_attributes, axs, num_bins):
 
@@ -115,4 +148,9 @@ if __name__ == "__main__":
     subjects = ['News', 'politics', 'Government News', 'left-news', 'US_News', 'Middle-east']
     
 
-    # sa.analyze_and_plot('title', analyzer, verbose=True)
+    sa.analyze_and_plot('text', analyzer, verbose=True)
+
+    # sa.append_and_save_df(df_test, 'data/test_sentiment.csv', 'col1', analyzer, verbose=True)
+
+    # sa.append_and_save_df(fake_df, 'data/Fake_sentiment.csv', 'text', analyzer, verbose=True)
+    # sa.append_and_save_df(true_df, 'data/True_sentiment.csv', 'text', analyzer, verbose=True)
